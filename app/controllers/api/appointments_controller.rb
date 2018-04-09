88117bc8697f8c8a7d8f4ad4719ace992ee06170
Ignat Skoryh <ignat.skoryh@gmail.com>
2018-04-09 21:07:38 +0300

    Appointments created from API have pending status

--- app/controllers/api/appointments_controller.rb
+++ app/controllers/api/appointments_controller.rb
@@ -3,6 +3,7 @@ class Api::AppointmentsController < ActionController::API
   def index
     # TODO: authorize and find user
     @user = User.find(1)
+    # TODO: filter by date
     @appointments = @user.appointments
     render json: @appointments, status: :ok
   end
@@ -10,7 +11,7 @@ class Api::AppointmentsController < ActionController::API
   def create
     # TODO: authorize and find user
     @user = User.find(1)
-    @appointment = Appointment.create(params.permit(:title, :when).merge(user_id: @user.id))
+    @appointment = Appointment.create(params.permit(:title, :when).merge(user_id: @user.id, status: :pending))
     render json: @appointment, status: :ok
   end
 
