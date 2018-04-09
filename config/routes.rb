ec2593130e17c91ef034195df6f7dc8fa046094d
Ignat Skoryh <ignat.skoryh@gmail.com>
2018-04-09 13:48:40 +0300

    Add Appointments controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
-  get 'home/index'
-
   devise_for :users
 
+  resources :appointments
+
+  get 'home/index'
   root to: 'home#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
