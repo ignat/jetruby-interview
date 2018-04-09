6199b296ba74158b1e384431e5b04136de22bf78
Ignat Skoryh <ignat.skoryh@gmail.com>
2018-04-09 12:59:28 +0300

    Add Home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+
+  get 'home/index'
+
   devise_for :users
 
-  # TODO: set default home page for devise redirects
-  # root to: 'home#index'
+  root to: 'home#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
