29d8b1da8e09681efc5e847c25dfc9fa729908c2
Ignat Skoryh <ignat.skoryh@gmail.com>
2018-04-09 11:29:14 +0300

    Install Devise auth system
    Install Devise authentication/authorization.
    Setup Postgresql database.
    Run migrations and Devise setup.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  # TODO: set default home page for devise redirects
+  # root to: 'home#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
