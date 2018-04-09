5d20f6716ae4d9c568c1ff8375d44ed5e26dbce7
Ignat Skoryh <ignat.skoryh@gmail.com>
2018-04-09 17:35:19 +0300

    Add Appointments actions and views

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,22 +1,37 @@
 class AppointmentsController < ApplicationController
 
+  before_action :authenticate_user!
+
   def index
     @appointments = current_user.appointments
   end
 
   def new
+    @appointment = Appointment.new
   end
 
   def create
+    @appointment = Appointment.create(params.require(:appointment).permit(:title, :when, :status).merge(user_id: current_user.id))
+    flash[:notice] = "Successfully created a new appointment"
+    redirect_to appointments_url
   end
 
   def edit
+    @appointment = Appointment.where(id: params[:id], user_id: current_user.id).first
   end
 
   def update
+    @appointment = Appointment.where(id: params[:id], user_id: current_user.id).first
+    @appointment.update(params.require(:appointment).permit(:title, :when, :status))
+    flash[:notice] = "Successfully updated the appointment"
+    redirect_to appointments_url
   end
 
   def destroy
+    @appointment = Appointment.where(id: params[:id], user_id: current_user.id).first
+    @appointment.delete
+    flash[:notice] = "Successfully deleted the appointment"
+    redirect_to appointments_url
   end
 
 end
