c0e06ff64a8871d36acdcfad026d0848a51f9342
Ignat Skoryh <ignat.skoryh@gmail.com>
2018-04-11 12:35:23 +0300

    Implement API methods
    Support optional date parameter from appointments listing.
    Support optional reminder parameter for new appointment.
    Implement authentication/authorization via API key.

--- app/controllers/api/appointments_controller.rb
+++ app/controllers/api/appointments_controller.rb
@@ -1,17 +1,27 @@
 class Api::AppointmentsController < ActionController::API
 
   def index
-    # TODO: authorize and find user
-    @user = User.find(1)
-    # TODO: filter by date
-    @appointments = @user.appointments
+    api_key = request.headers['Authorization']
+    return head :unauthorized if api_key.blank?
+    @user = User.find_by_api_key(api_key)
+    return head :unauthorized if @user.nil?
+    if params[:date].present?
+      @appointments = @user.appointments.where(["date(\"when\") = ?", params[:date]])
+    else
+      @appointments = @user.appointments
+    end
     render json: @appointments, status: :ok
   end
 
   def create
-    # TODO: authorize and find user
-    @user = User.find(1)
+    api_key = request.headers['Authorization']
+    return head :unauthorized if api_key.blank?
+    @user = User.find_by_api_key(api_key)
+    return head :unauthorized if @user.nil?
     @appointment = Appointment.create(params.permit(:title, :when).merge(user_id: @user.id, status: :pending))
+    if params[:reminder].present?
+      @appointment.reminders.create(when: params[:reminder])
+    end
     render json: @appointment, status: :created
   end
 
